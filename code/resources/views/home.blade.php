3efc57c3e356ad6282188befed6d105e9bffc905
Adamo Avelino <adamo.avelino@gmail.com>
2021-04-16 01:05:32 -0300

    Atualização do axios para 0.21 e inclusão de alguns cars

--- code/resources/views/home.blade.php
+++ code/resources/views/home.blade.php
@@ -3,7 +3,36 @@
 @section('content')
 <section class="container mt-5">
 	<div class="row">
-		<h1>Espaço para montar o dashboard</h1>
+		<div class="col-md-12">
+			<h1>Espaço para montar o dashboard</h1>
+		</div>
+		<div class="col-md-4">
+			<div class="card" style="background-color:cadetblue; border-color:darkblue;">
+			  <img class="card-img-top" src="holder.js/100x180/" alt="">
+			  <div class="card-body">
+				<h4 class="card-title">Title</h4>
+				<p class="card-text">Text</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card" style="background-color:cadetblue; border-color:darkblue;">
+			  <img class="card-img-top" src="holder.js/100x180/" alt="">
+			  <div class="card-body">
+				<h4 class="card-title">Title</h4>
+				<p class="card-text">Text</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="card" style="background-color:cadetblue; border-color:darkblue;">
+			  <img class="card-img-top" src="holder.js/100x180/" alt="">
+			  <div class="card-body">
+				<h4 class="card-title">Title</h4>
+				<p class="card-text">Text</p>
+			  </div>
+			</div>
+		</div>
 	</div>
 </section>
 @endsection
